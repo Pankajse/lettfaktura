f549bca23d72252e78562364d883f6b587e09561
Pankaj42se <pankaj42se@gmail.com>
2025-10-21 23:52:17 +0530

    make middleware change

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -2,11 +2,13 @@ const jwt = require("jsonwebtoken");
 const prisma = require("../lib/prisma");
 
 
-const authMiddleware = async(req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
-    if (!token) {
-        return res.status(401).json({ message: "No token provided" });
+const authMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: "Authorization token is missing or invalid" });
     }
+    const token = authHeader.split(" ")[1];
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.id;
@@ -21,4 +23,4 @@ const authMiddleware = async(req, res, next) => {
     }
 };
 
-module.exports = {authMiddleware};
+module.exports = { authMiddleware };
